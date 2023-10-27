6723b105e51d9dea02d6dba96f5048e50262e3d9
hobee <lzqpublic@163.com>
2023-10-27 17:07:24 +0800

    add

--- src/core.ts
+++ src/core.ts
@@ -19,20 +19,31 @@ export function getNextNumber(cntNums: number[], headerLevel: number): number[] 
 }
 
 export function isNeedInsertNumber(text: string, splitor: string): boolean {
-    if (text.contains(splitor)) return false; // '## splitor text
-    return true;
+    // '## header' true
+    // '## 1.1 splitor header' false
+    if (splitor == ' ') return text.split(splitor).length === 2;
+    else return !text.contains(splitor);
 }
 
 export function isNeedUpdateNumber(nextNumsStr: string, text: string, splitor: string): boolean {
-    let cntNumsStr = text.split(splitor)[0].split(' ')[0];
+    let cntNumsStr: string;
+    if (splitor == ' ') {cntNumsStr = text.split(splitor)[1];}
+    else {cntNumsStr = text.split(splitor)[0].split(' ')[0];}
     return nextNumsStr !== cntNumsStr;
 }
 
 export function removeHeaderNumber(text: string, splitor: string): string {
     // remove '1.1 splitor' from '## 1.1 splitor text'
-    if (!text.contains(splitor)) return text;
-    const sharp = text.split(splitor)[0].split(' ')[0];
-    return sharp + ' ' + text.split(splitor)[1];
+    let sharp: string, header: string;
+    if (splitor == ' ') {
+        sharp = text.split(splitor)[0];
+        header = text.split(splitor)[2];
+    } else {
+        if (!text.contains(splitor)) return text;
+        sharp = text.split(splitor)[0].split(' ')[0];
+        header = text.split(splitor)[1];
+    }
+    return sharp + ' ' + header;
 }
 
 export function isHeader(text: string): boolean {
