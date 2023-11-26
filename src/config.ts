42660db70cb78242d0aeb44255959207747aba1b
hobee <lzqpublic@163.com>
2023-11-27 00:48:13 +0800

    formate code

--- src/config.ts
+++ src/config.ts
@@ -1,48 +1,51 @@
-import { Editor } from 'obsidian';
-import { HeaderEnhancerSettings } from './setting';
-import { getAutoNumberingYaml } from './utils';
+import { Editor } from "obsidian";
+import { HeaderEnhancerSettings } from "./setting";
+import { getAutoNumberingYaml } from "./utils";
 
 export interface AutoNumberingConfig {
-    state: boolean;
-    startLevel: number;
-    maxLevel: number;
-    startNumber: number;
-    separator: string;
+	state: boolean;
+	startLevel: number;
+	maxLevel: number;
+	startNumber: number;
+	separator: string;
 }
 
-export function getAutoNumberingConfig(setting: HeaderEnhancerSettings, editor: Editor): AutoNumberingConfig {
-    const config: AutoNumberingConfig = {
-        state: setting.isAutoNumbering,
-        startLevel: setting.startHeaderLevel,
-        maxLevel: setting.maxHeaderLevel,
-        startNumber: parseInt(setting.autoNumberingStartNumber),
-        separator: setting.autoNumberingSeparator
-    }
+export function getAutoNumberingConfig(
+	setting: HeaderEnhancerSettings,
+	editor: Editor
+): AutoNumberingConfig {
+	const config: AutoNumberingConfig = {
+		state: setting.isAutoNumbering,
+		startLevel: setting.startHeaderLevel,
+		maxLevel: setting.maxHeaderLevel,
+		startNumber: parseInt(setting.autoNumberingStartNumber),
+		separator: setting.autoNumberingSeparator,
+	};
 
-    if (setting.isUseYaml) {
-        const yaml = getAutoNumberingYaml(editor)
-        if (yaml === '') return config
+	if (setting.isUseYaml) {
+		const yaml = getAutoNumberingYaml(editor);
+		if (yaml === "") return config;
 
-        for (const item of yaml) {
-            const [key, value] = item.split(' ')
-            switch (key) {
-                case 'state':
-                    config.state = value == "on" ? true : false
-                    break
-                case 'start-level':
-                    config.startLevel = parseInt(value[1])
-                    break
-                case 'max-level':
-                    config.maxLevel = parseInt(value[1])
-                    break
-                case 'start-number':
-                    config.startNumber = parseInt(value)
-                    break
-                case 'separator':
-                    config.separator = value
-                    break
-            }
-        }
-    }
-    return config
+		for (const item of yaml) {
+			const [key, value] = item.split(" ");
+			switch (key) {
+				case "state":
+					config.state = value == "on" ? true : false;
+					break;
+				case "start-level":
+					config.startLevel = parseInt(value[1]);
+					break;
+				case "max-level":
+					config.maxLevel = parseInt(value[1]);
+					break;
+				case "start-number":
+					config.startNumber = parseInt(value);
+					break;
+				case "separator":
+					config.separator = value;
+					break;
+			}
+		}
+	}
+	return config;
 }
